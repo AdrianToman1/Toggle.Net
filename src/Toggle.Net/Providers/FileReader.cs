f92dd10a9c281f9dab86d954ca20e90aaf594a7f
Adrian Toman <adrian@adriantoman.com>
2024-06-08 15:12:14 +1000

    Add mockable file reader

---
+++ src/Toggle.Net/Providers/FileReader.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace Toggle.Net.Providers
+{
+    /// <summary>
+    ///     An injectable wrapper for selected <see cref="File">System.IO.File</see> functionality, suitable for mocking.
+    /// </summary>
+    public interface IFileReader
+    {
+        /// <inheritdoc cref="File.ReadAllText(string)" />
+        string ReadAllText(string path);
+    }
+
+    /// <inheritdoc />
+    public class FileReader : IFileReader
+    {
+        /// <inheritdoc />
+        public string ReadAllText(string path)
+        {
+            return File.ReadAllText(path);
+        }
+    }
+}
