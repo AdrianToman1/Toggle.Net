5ef29f409c4f15bddb5c379ee4ca53b66b26dfdf
Adrian Toman <adrian@adriantoman.com>
2024-06-15 20:04:34 +1000

    Add basic console app example

---
+++ examples/ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toggle.Net;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var toggleChecker = ToggleChecker.FromJsonFile("toggles.json");
+
+            PrintFeatureStatus(toggleChecker, "enabledFeature");
+            PrintFeatureStatus(toggleChecker, "disabledFeature");
+            PrintFeatureStatus(toggleChecker, "unknownFeature");
+
+            Console.ReadLine();
+        }
+
+        private static void PrintFeatureStatus(IToggleChecker toggleChecker, string featureName)
+        {
+            Console.WriteLine($"Is {featureName} enabled? {toggleChecker.IsEnabled(featureName)}");
+        }
+    }
+}
